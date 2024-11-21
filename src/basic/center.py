efbae8badc56f6db6a211a3f2e25424b8405d6d5
Frieda.Hu <www778878net@189.cn>
2024-11-21 21:15:23 +0800

    fix

--- src/basic/center.py
+++ src/basic/center.py
@@ -34,7 +34,7 @@ class Center():
     async def run(self):        
         """主循环逻辑"""
         await self.optimizer.test()
-        await self.runtask.run()
+        #await self.runtask.run()
         
         #await self.test()
         #await self.optimizer.run()        
