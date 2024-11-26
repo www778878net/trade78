2369c9a9179d5c9500fcb4a3fe88d6b986be0468
Frieda.Hu <www778878net@189.cn>
2024-11-26 22:25:05 +0800

    fix

--- src/center/optimizer.py
+++ src/center/optimizer.py
@@ -57,7 +57,7 @@ class Optimizer:
             loop.close()  # 关闭事件循环
     async def __run(self):
         up=UpInfo.getMaster() 
-        up.getnumber=3#这里直接是调整减几天 每次只获取一个
+        up.getnumber=7#这里直接是调整减几天 每次只获取一个
         dt=await up.send_back("apistock/stock/stock_trade/mForOptimizetimeAll")  
         #print (dt)
         isAllok=True  
